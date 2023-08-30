da70003d7df599d60122b94f8c39c3424b0c11eb
no.oneho <oneho.dev@gmail.com>
2023-08-30 12:40:55 +0900

    git init

---
+++ src/main/java/com/supercoding/hanyipman/HanYipManApplication.java
@@ -0,0 +1,13 @@
+package com.supercoding.hanyipman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanYipManApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanYipManApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/supercoding/hanyipman/HanYipManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supercoding.hanyipman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanYipManApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
