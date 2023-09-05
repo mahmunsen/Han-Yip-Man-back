806302f28dac3ffde34e32b6a2b3b702c4417bac
no.oneho <oneho.dev@gmail.com>
2023-09-06 05:31:02 +0900

    Fix: Cors 수정

--- src/main/java/com/supercoding/hanyipman/config/security/CorsFilterConfig.java
+++ src/main/java/com/supercoding/hanyipman/config/security/CorsFilterConfig.java
@@ -5,20 +5,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsFilterConfig {
+public class CorsFilterConfig{
+
+    @Bean
+    public CorsConfiguration corsConfiguration() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*"); // 모든 오리진 허용
+        configuration.addAllowedHeader("*"); // 모든 헤더 허용
+        configuration.addExposedHeader("X-API-VERSION"); // 커스텀 헤더를 노출시킴
+        configuration.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
+        return configuration;
+    }
+
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-
-        source.registerCorsConfiguration("/api/**",config);
+        source.registerCorsConfiguration("/**", corsConfiguration());
         return new CorsFilter(source);
-
     }
 }
