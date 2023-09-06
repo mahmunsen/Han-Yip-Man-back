2fe2c75aeb7cf5575a45eb7af50b776964812fe9
no.oneho <oneho.dev@gmail.com>
2023-09-06 12:42:22 +0900

    Feat: QueryDsl에 맞게 repo 구조화 후 위치기반 음식점 조회 QueryDsl 적용

--- src/main/java/com/supercoding/hanyipman/repository/ShopRepository.java
+++
@@ -1,7 +0,0 @@
-package com.supercoding.hanyipman.repository;
-
-import com.supercoding.hanyipman.entity.Shop;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShopRepository extends JpaRepository<Shop, Long> {
-}
