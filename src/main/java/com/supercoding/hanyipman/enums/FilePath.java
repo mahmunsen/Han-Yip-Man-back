e21eb53782df4fb938de87acc90be9ab1afc242d
G1-H <hwangjiwon1992@gmail.com>
2023-09-06 17:54:47 +0900

    Refactor : FilePath에 리뷰 위한 enum 상수 추가
    REVIEW_DIR 추가

--- src/main/java/com/supercoding/hanyipman/enums/FilePath.java
+++ src/main/java/com/supercoding/hanyipman/enums/FilePath.java
@@ -3,7 +3,8 @@ package com.supercoding.hanyipman.enums;
 public enum FilePath {
     TEST_DIR("user/image/"),
     SEPARATE_POINT(".com/"),
-    SHOP_DIR("/shop/");
+    SHOP_DIR("/shop/"),
+    REVIEW_DIR("/review/");
 
     FilePath(String path) {
         this.path = path;
