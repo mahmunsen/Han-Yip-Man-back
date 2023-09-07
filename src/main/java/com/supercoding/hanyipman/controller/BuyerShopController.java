7d2ebc8580bcfc56ef378aa1891eeace135ab485
G1-H <hwangjiwon1992@gmail.com>
2023-09-07 17:30:38 +0900

    Feat : BuyerShop 컨트롤러, 상점 리뷰 조회 메서드 추가
    상점 리뷰 조회 메서드 추가

--- src/main/java/com/supercoding/hanyipman/controller/BuyerShopController.java
+++ src/main/java/com/supercoding/hanyipman/controller/BuyerShopController.java
@@ -1,21 +1,21 @@
 package com.supercoding.hanyipman.controller;
 
 
+import com.supercoding.hanyipman.dto.reivew.request.ViewShopReviewsRequest;
+import com.supercoding.hanyipman.dto.reivew.response.ViewShopReviewsResponse;
 import com.supercoding.hanyipman.dto.shop.buyer.request.ViewShopListRequest;
 import com.supercoding.hanyipman.dto.shop.buyer.response.ViewShopListResponse;
 import com.supercoding.hanyipman.dto.user.CustomUserDetail;
 import com.supercoding.hanyipman.dto.vo.Response;
 import com.supercoding.hanyipman.service.BuyerShopService;
+import com.supercoding.hanyipman.service.ReviewService;
 import com.supercoding.hanyipman.utils.ApiUtils;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/buyer-shops")
 @RestController
@@ -25,12 +25,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class BuyerShopController {
 
     private final BuyerShopService buyerShopService;
+    private final ReviewService reviewService;
 
-    @GetMapping(value="", headers="X-API-VERSION=1")
+    @GetMapping(value = "", headers = "X-API-VERSION=1")
     public Response<ViewShopListResponse> findShopList(ViewShopListRequest viewShopListRequest,
                                                        @AuthenticationPrincipal CustomUserDetail customUserDetail) {
 
         return ApiUtils.success(HttpStatus.OK, "리스트 조회 성공", buyerShopService.findShopList(viewShopListRequest, customUserDetail));
     }
 
+    @GetMapping(value = "/{shopId}/reviews", headers ="X-API-VERSION=1")
+    public Response<ViewShopReviewsResponse> viewShopReviews(@PathVariable String shopId,
+                                                             @RequestBody ViewShopReviewsRequest viewShopReviewsRequest) {
+
+        return ApiUtils.success(HttpStatus.OK, "해당 가게에 대한 리뷰 조회에 성공했습니다.", reviewService.viewShopReviews(shopId, viewShopReviewsRequest));
+    }
 }
