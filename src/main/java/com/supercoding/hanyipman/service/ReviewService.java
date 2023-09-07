663be81d8b7314c6271140da0c43b7797c784494
G1-H <hwangjiwon1992@gmail.com>
2023-09-07 17:42:28 +0900

    Feat : ReviewService 리뷰 조회 메서드 추가
    별점 정보 없을 경우, 커서 정보 없을 경우(첫 조회) 구분하여 조회 가능

--- src/main/java/com/supercoding/hanyipman/service/ReviewService.java
+++ src/main/java/com/supercoding/hanyipman/service/ReviewService.java
@@ -1,6 +1,9 @@
 package com.supercoding.hanyipman.service;
 
 import com.supercoding.hanyipman.dto.reivew.request.RegisterReviewRequest;
+import com.supercoding.hanyipman.dto.reivew.request.ViewShopReviewsRequest;
+import com.supercoding.hanyipman.dto.reivew.response.ShopReview;
+import com.supercoding.hanyipman.dto.reivew.response.ViewShopReviewsResponse;
 import com.supercoding.hanyipman.dto.user.CustomUserDetail;
 import com.supercoding.hanyipman.entity.Buyer;
 import com.supercoding.hanyipman.entity.Review;
@@ -14,11 +17,17 @@ import com.supercoding.hanyipman.repository.ReviewRepository;
 import com.supercoding.hanyipman.repository.Shop.ShopRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -34,9 +43,10 @@ public class ReviewService {
         Long userId = userDetail.getUserId();
         Buyer buyer = validateUser(userId);
 
-        Shop shop = shopRepository.findById(registerReviewRequest.getShopId()).orElseThrow(()->new CustomException(ReviewErrorCode.STORE_NOT_FOUND));
+        Shop shop = validateShop(registerReviewRequest.getShopId());
 
-        if(reviewRepository.existsByBuyerAndShop(buyer, shop)) throw new CustomException(ReviewErrorCode.REGISTERED_BEFORE);
+        if (reviewRepository.existsByBuyerAndShop(buyer, shop))
+            throw new CustomException(ReviewErrorCode.REGISTERED_BEFORE);
 
         Review reviewToRegister = Review.builder()
                 .shop(shop)
@@ -52,9 +62,37 @@ public class ReviewService {
 
     }
 
+    public ViewShopReviewsResponse viewShopReviews(String shopId, ViewShopReviewsRequest viewShopReviewsRequest) {
+
+        Shop shop = validateShop(Long.valueOf(shopId));
+        Integer score = viewShopReviewsRequest.getReviewScore();
+        Instant cursor = viewShopReviewsRequest.getCursor();
+
+
+        Pageable pageable = PageRequest.of(0, viewShopReviewsRequest.getSize(), Sort.Direction.DESC, "createdAt");
+        List<ShopReview> shopReviewList;
+        if (score == null) {
+           shopReviewList = getShopReviewListWhenScoreNull(shop, cursor ,pageable);
+        }else{
+            shopReviewList = getShopReviewListWithScore(shop, score, cursor, pageable);
+        }
+
+        Instant returnCursor = shopReviewList.isEmpty() ? null : shopReviewList.get(shopReviewList.size() - 1).getCreatedAt();
+
+        return ViewShopReviewsResponse.builder()
+                .cursor(returnCursor)
+                .shopReviewsList(shopReviewList)
+                .build();
+    }
+
     private Buyer validateUser(Long userId) {
         return buyerRepository.findBuyerByUserId(userId)
                 .orElseThrow(() -> new CustomException(ReviewErrorCode.NOT_PROPER_USER));
+    }
+
+    private Shop validateShop(Long shopId) {
+        return shopRepository.findById(shopId)
+                .orElseThrow(() -> new CustomException(ReviewErrorCode.STORE_NOT_FOUND));
     }
 
     private String uploadImageFile(MultipartFile multipartFile, Review review) {
@@ -62,10 +100,36 @@ public class ReviewService {
             if (multipartFile != null) {
                 return awsS3Service.uploadImage(multipartFile, FilePath.REVIEW_DIR.getPath() + review.getId());
             }
-        }catch (IOException e) {
+        } catch (IOException e) {
             throw new CustomException(FileErrorCode.FILE_UPLOAD_FAILED);
         }
         return null;
+    }
+
+    private List<ShopReview> getShopReviewListWhenScoreNull(Shop shop, Instant cursor, Pageable pageable) {
+        List<ShopReview> shopReviewList;
+        if (cursor == null) {
+            shopReviewList = reviewRepository.findAllByShopOrderByCreatedAtDesc(shop, pageable).getContent().stream()
+                    .map(review -> Review.toShopReview(review)).collect(Collectors.toList());
+            return shopReviewList;
+        }
+
+        shopReviewList = reviewRepository.findAllByShopAndCreatedAtLessThanOrderByCreatedAtDesc(shop, cursor, pageable).stream()
+                .map(review -> Review.toShopReview(review)).collect(Collectors.toList());
+        return shopReviewList;
+    }
+
+    private List<ShopReview> getShopReviewListWithScore(Shop shop, Integer score, Instant cursor,  Pageable pageable) {
+        List<ShopReview> shopReviewList;
+        if (cursor == null) {
+            shopReviewList = reviewRepository.findAllByShopAndScoreOrderByCreatedAtDesc(shop, score, pageable).getContent().stream()
+                    .map(review -> Review.toShopReview(review)).collect(Collectors.toList());
+            return shopReviewList;
+        }
+
+        shopReviewList = reviewRepository.findAllByShopAndScoreAndCreatedAtLessThanOrderByCreatedAtDesc(shop, score, cursor, pageable).stream()
+                .map(review -> Review.toShopReview(review)).collect(Collectors.toList());
+        return shopReviewList;
     }
 
 }
