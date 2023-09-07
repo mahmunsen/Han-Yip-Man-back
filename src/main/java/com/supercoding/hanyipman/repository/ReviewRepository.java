ca0c5343e8c1438d2898448dd91bd3cb96c5ddc7
G1-H <hwangjiwon1992@gmail.com>
2023-09-07 17:41:07 +0900

    Feat : ReviewRepository 리뷰 조회 관련 쿼리 메서드 추가
    리뷰 조회 관련 쿼리 메서드 추가

--- src/main/java/com/supercoding/hanyipman/repository/ReviewRepository.java
+++ src/main/java/com/supercoding/hanyipman/repository/ReviewRepository.java
@@ -3,8 +3,21 @@ package com.supercoding.hanyipman.repository;
 import com.supercoding.hanyipman.entity.Buyer;
 import com.supercoding.hanyipman.entity.Review;
 import com.supercoding.hanyipman.entity.Shop;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.Instant;
+import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     boolean existsByBuyerAndShop(Buyer buyer, Shop shop);
+
+    Page<Review> findAllByShopOrderByCreatedAtDesc(Shop shop, Pageable pageable);
+
+    List<Review> findAllByShopAndCreatedAtLessThanOrderByCreatedAtDesc(Shop shop, Instant createdAt, Pageable pageable);
+
+    Page<Review> findAllByShopAndScoreOrderByCreatedAtDesc(Shop shop,Integer score, Pageable pageable);
+
+    List<Review> findAllByShopAndScoreAndCreatedAtLessThanOrderByCreatedAtDesc(Shop shop,Integer score, Instant createdAt, Pageable pageable);
 }
