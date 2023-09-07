11d2770f739ad36b5d4d146b50e9de9e10334826
Woojin Lee <69797420+zmfpdl64@users.noreply.github.com>
2023-09-07 20:54:02 +0900

    refactor: dto<-> service 의존성 분리
    가독성 측면에서 반환타입을 명시적으로 선언함

--- src/main/java/com/supercoding/hanyipman/controller/CartController.java
+++ src/main/java/com/supercoding/hanyipman/controller/CartController.java
@@ -1,6 +1,7 @@
 package com.supercoding.hanyipman.controller;
 
 import com.supercoding.hanyipman.dto.cart.request.RegisterCartRequest;
+import com.supercoding.hanyipman.dto.cart.request.UpdateCartRequest;
 import com.supercoding.hanyipman.dto.cart.response.ViewCartResponse;
 import com.supercoding.hanyipman.dto.user.CustomUserDetail;
 import com.supercoding.hanyipman.dto.vo.CustomPageable;
@@ -9,19 +10,12 @@ import com.supercoding.hanyipman.service.CartService;
 import com.supercoding.hanyipman.utils.ApiUtils;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -35,29 +29,39 @@ public class CartController {
 
     @Operation(summary = "장바구니 등록", description = "사용자가 메뉴, 옵션을 담은 장바구니를 서버에 저장한다. 옵션에서 \"\"를 제거하고 실행해주세요")
     @PostMapping
-    public Response<?> registerCart(@RequestBody RegisterCartRequest request,
+    public Response<Void> registerCart(@RequestBody RegisterCartRequest request,
                                @AuthenticationPrincipal CustomUserDetail userDetail){
-        cartService.registerCart(userDetail.getUserId(), request);
+        cartService.registerCart(
+                userDetail.getUserId(),
+                request.getShopId(),
+                request.getMenuId(),
+                request.getOptions(),
+                request.getAmount()
+        );
         return ApiUtils.success(HttpStatus.CREATED, "장바구니에 성공적으로 담았습니다.", null);
     }
 
 //    @Operation(summary = "장바구니 조회", description = "사용자가 담았던 장바구니 리스트를 반환한다.")
 //    @GetMapping
-//    public Response<?> registerCart(Pageable pageable){
+//    public Response<?> viewAllCartsV1(Pageable pageable){
 //        Page<ViewCartResponse> cartsResponse =  cartService.findUnpaidCartsV1(pageable);
 //        return ApiUtils.success(HttpStatus.OK, "장바구니를 성공적으로 가져왔습니다.", cartsResponse);
 //    }
 
     @Operation(summary = "장바구니 조회", description = "사용자가 담았던 장바구니 리스트를 반환한다.")
     @GetMapping
-    public Response<?> registerCart(CustomPageable pageable){
-        List<ViewCartResponse> cartsResponse =  cartService.findUnpaidCartsV2(pageable);
+    public Response<List<ViewCartResponse>> viewAllCartsV2(CustomPageable pageable){
+        List<ViewCartResponse> cartsResponse =  cartService.findUnpaidCartsAndOptionItemsV2(pageable);
         return ApiUtils.success(HttpStatus.OK, "장바구니를 성공적으로 가져왔습니다.", cartsResponse);
     }
 
-    @GetMapping("/test")
-    public Response<?> test(){
-        return ApiUtils.success(HttpStatus.OK, "테스트", new ArrayList<>());
+    @Operation(summary = "장바구니 수정", description = "기존 장바구니의 수량을 변경한다.")
+    @PatchMapping
+    public Response<Void> updateCart(@RequestBody UpdateCartRequest request,
+                                  @AuthenticationPrincipal CustomUserDetail auth){
+        cartService.updateCart(request.getCartId(), request.getAmount(), auth.getUserId());
+        return ApiUtils.success(HttpStatus.OK, "장바구니 수량을 성공적으로 변경했습니다.", null);
     }
+
 
 }
--- src/main/java/com/supercoding/hanyipman/repository/cart/CartRepository.java
+++ src/main/java/com/supercoding/hanyipman/repository/cart/CartRepository.java
@@ -1,4 +1,4 @@
-package com.supercoding.hanyipman.repository;
+package com.supercoding.hanyipman.repository.cart;
 
 import com.supercoding.hanyipman.entity.Cart;
 import org.springframework.data.domain.Page;
@@ -11,6 +11,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
@@ -22,4 +23,12 @@ public interface CartRepository extends JpaRepository<Cart, Long> {
             "and b.id =:buyerId")
     Page<Cart> findCartsByUnpaidCart(@Param("buyerId") Long buyerId, Pageable pageable);
 
+    @Query("SELECT c FROM Cart c where c.id =:cartId")
+    Optional<Cart> findCartByCartId(@Param("cartId") Long cartId);
+
+    @Query("SELECT c FROM Cart c " +
+            "JOIN fetch c.shop s " +
+            "JOIN fetch c.buyer b " +
+            "WHERE b.id =:buyerId")
+    List<Cart> findCartsByBuyerId(@Param("buyerId") Long buyerId);
 }
