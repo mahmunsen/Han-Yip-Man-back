3bfc8c00981480c48b6430fdbe94b015d4770e83
no.oneho <oneho.dev@gmail.com>
2023-09-08 14:47:11 +0900

    Feat: 가게 상세 조회를 위한 응답DTO 개발

---
+++ src/main/java/com/supercoding/hanyipman/dto/shop/seller/response/ShopDetailResponse.java
@@ -0,0 +1,41 @@
+package com.supercoding.hanyipman.dto.shop.seller.response;
+
+import com.supercoding.hanyipman.entity.Shop;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ShopDetailResponse {
+
+    private String shopName;
+    private String thumbnailUrl;
+    private String bannerUrl;
+    private String shopPhone;
+    private String categoryName;
+    private String address;
+    private String detailAddress;
+    private String businessNumber;
+    private Integer minOrderPrice;
+    private String shopDescription;
+
+    public static ShopDetailResponse from(Shop shop) {
+        return ShopDetailResponse.builder()
+                .shopName(shop.getName())
+                .thumbnailUrl(shop.getThumbnail())
+                .bannerUrl(shop.getBanner())
+                .shopPhone(shop.getPhoneNum())
+                .categoryName(shop.getCategory().getName())
+                .address(shop.getAddress().getAddress())
+                .detailAddress(shop.getAddress().getDetailAddress())
+                .businessNumber(shop.getBusinessNumber())
+                .minOrderPrice(shop.getMinOrderPrice())
+                .shopDescription(shop.getDescription())
+                .build();
+    }
+
+}
