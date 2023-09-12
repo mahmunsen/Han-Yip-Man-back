4a6e812de78913e52fc05175d6060d144e5810de
G1-H <hwangjiwon1992@gmail.com>
2023-09-12 16:09:21 +0900

    Feat : 웹 소켓 컨트롤러 생성

---
+++ src/main/java/com/supercoding/hanyipman/socket/SocketController.java
@@ -0,0 +1,63 @@
+package com.supercoding.hanyipman.socket;
+
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.listener.ConnectListener;
+import com.corundumstudio.socketio.listener.DataListener;
+import com.corundumstudio.socketio.listener.DisconnectListener;
+import com.supercoding.hanyipman.dto.websocket.ChatMessage;
+import com.supercoding.hanyipman.dto.websocket.OrderStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class SocketController {
+
+    private final SocketIOServer server;
+    private final SocketService socketService;
+
+    public SocketController(SocketIOServer server, SocketService socketService) {
+        this.server = server;
+        this.socketService = socketService;
+        server.addConnectListener(onConnected());
+        server.addDisconnectListener(onDisconnected());
+        server.addEventListener("send_message", ChatMessage.class, onChatReceived());
+        server.addEventListener("send_order", OrderStatus.class, onOrderStatusReceived());
+
+
+    }
+
+    private DataListener<ChatMessage> onChatReceived() {
+        return (senderClient, data, ackSender) -> {
+            log.info(data.toString());
+            socketService.sendMessage(data.getRoom(),"get_message", senderClient, data.getMessage());
+        };
+    }
+
+    private DataListener<OrderStatus> onOrderStatusReceived() {
+        return (senderClient, data, ackSender) -> {
+            log.info(data.toString());
+            socketService.sendOrderStatus(data.getRoom(),"get_order_status_response", senderClient, data.getOrderType());
+        };
+    }
+
+    private ConnectListener onConnected() {
+        return (client) -> {
+            String room = client.getHandshakeData().getSingleUrlParam("room");
+            log.info("room : " + room);
+
+            client.joinRoom(room);
+            log.info("Socket ID[{}]  Connected to socket", client.getSessionId().toString());
+        };
+
+    }
+
+
+
+    private DisconnectListener onDisconnected() {
+        return (client) -> {
+            log.info("Client[{}] - Disconnected from socket", client.getSessionId().toString());
+        };
+    }
+
+}
