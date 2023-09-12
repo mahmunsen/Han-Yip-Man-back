3053b327155e79c46ab7b2dba3d55f632962e08b
mahmunsen <tjs339@gmail.com>
2023-09-12 15:02:44 +0900

    refactor: 기존에 orderTest와 연동부분 -> order 로 변경

--- src/main/java/com/supercoding/hanyipman/controller/payment/IamportController.java
+++ src/main/java/com/supercoding/hanyipman/controller/payment/IamportController.java
@@ -9,10 +9,11 @@ import com.supercoding.hanyipman.dto.payment.response.iamport.GetOnePaymentRespo
 import com.supercoding.hanyipman.dto.payment.response.iamport.PaymentPrepareResponse;
 import com.supercoding.hanyipman.dto.vo.Response;
 import com.supercoding.hanyipman.security.JwtToken;
+import com.supercoding.hanyipman.service.OrderService;
 import com.supercoding.hanyipman.service.PaymentService;
 import com.supercoding.hanyipman.utils.ApiUtils;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,12 +26,12 @@ import org.springframework.web.bind.annotation.*;
 public class IamportController {
 
     private final PaymentService paymentService;
+    private final OrderService orderService;
 
-    /* todo 엑세스 토큰, 버전 02 (입력폼 없음) */
-    @ApiOperation(value = "(아임포트) 엑세스 토큰 API ", nickname = "아임포트로부터 엑세스 토큰을 발급받는 API")
+    /* todo 엑세스 토큰 */
+    @Operation(summary = "(아임포트) 엑세스 토큰 API ", description = "아임포트로부터 엑세스 토큰을 발급받는 API")
     @PostMapping(path = "/getToken", headers = "X-API-VERSION=1")
     public Response<Object> getToken(@RequestBody AccessTokenRequest accessTokenRequest) {
-
         Long orderId = accessTokenRequest.getOrderId();
         String accessToken = paymentService.getToken(JwtToken.user(), orderId);
 
@@ -38,8 +39,8 @@ public class IamportController {
     }
 
 
-    /* todo 결제사전검증, 버전 02 (토큰 입력 없음) */
-    @ApiOperation(value = "(아임포트) 결제사전검증 API ", nickname = "아임포트에 결제번호와 결제예정금액을 사전에 등록하는 API")
+    /* todo 결제사전검증 (엑세스 토큰 발급과정 들어있음) */
+    @Operation(summary = "(아임포트) 결제사전검증 API ", description = "아임포트에 결제번호와 결제예정금액을 사전에 등록하는 API")
     @PostMapping(path = "/prepare", headers = "X-API-VERSION=1")
     public Response<PaymentPrepareResponse> paymentPrepare(@RequestBody RegisterPaymentRequest registerPaymentRequest) {
 
@@ -49,7 +50,7 @@ public class IamportController {
         return ApiUtils.success(HttpStatus.OK.value(), "결제사전검증에 성공하였습니다.", paymentPrepareResponse);
     }
 
-    @ApiOperation(value = "(아임포트) 결제내역 단건조회 API ", nickname = "아임포트에서 결제내역 단건을 조회 API")
+    @Operation(summary = "(아임포트) 결제내역 단건조회 API ", description = "아임포트에서 결제내역 단건을 조회 API")
     @GetMapping(path = "/{imp_uid}/{orderId}", headers = "X-API-VERSION=1")
     public Response<GetOnePaymentResponse> getOnePayment(@PathVariable("imp_uid") String imp_uid, @PathVariable("orderId") Long orderId) {
 
@@ -57,7 +58,7 @@ public class IamportController {
         return ApiUtils.success(HttpStatus.OK.value(), "결제내역 단건조회에 성공하였습니다.", getOnePaymentResponse);
     }
 
-    @ApiOperation(value = "(아임포트) 결제사후검증 API ", nickname = "(아임포트)실제결제된 금액과 (주문)결제예상금액이 맞는지 확인하는 API")
+    @Operation(summary = "(아임포트) 결제사후검증 API ", description = "(아임포트)실제결제된 금액과 (주문)결제예상금액이 맞는지 확인하는 API")
     @PostMapping(path = "/complete", headers = "X-API-VERSION=1")
     public Response<Object> verifyPayment(@RequestBody PostPaymentRequest postPaymentRequest) {
 
@@ -66,7 +67,7 @@ public class IamportController {
         return ApiUtils.success(HttpStatus.OK.value(), "결제사후검증에 성공하였습니다.", response.getBody());
     }
 
-    @ApiOperation(value = "(아임포트) 결제취소 API ", nickname = "승인된 결제를 취소하는 API")
+    @Operation(summary = "(아임포트) 승인 후 결제취소 API ", description = "승인된 결제를 취소/환불하는 API")
     @PostMapping(path = "/iamport/cancel", headers = "X-API-VERSION=1")
     public Response<Object> cancelPayment(@RequestBody CancelPaymentRequest cancelPaymentRequest) {
         CancelPaymentResponse cancelPaymentResponse = paymentService.cancelPayment(cancelPaymentRequest, JwtToken.user());
--- src/main/java/com/supercoding/hanyipman/repository/PaymentRepository.java
+++ src/main/java/com/supercoding/hanyipman/repository/PaymentRepository.java
@@ -1,19 +1,13 @@
 package com.supercoding.hanyipman.repository;
-
 import com.supercoding.hanyipman.entity.Order;
-import com.supercoding.hanyipman.entity.OrderTest;
 import com.supercoding.hanyipman.entity.Payment;
-import com.supercoding.hanyipman.entity.Seller;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 
 import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment,Long> {
-
-    Payment findPaymentByImpUidAndMerchantUid (String impUid, String merchantUid);
-    Optional<Payment> findPaymentByOrderTestId (OrderTest orderTest);
-    Payment findPaymentByOrderTestIdId (Long orderId);
+    Optional<Payment> findPaymentByOrder(Order order);
+    Payment findPaymentByOrderId(Long orderId);
 
 }
