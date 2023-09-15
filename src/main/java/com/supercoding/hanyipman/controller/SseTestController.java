ffa30de11ce21af82118fb57513939e6628fee3f
Woojin Lee <69797420+zmfpdl64@users.noreply.github.com>
2023-09-15 17:18:41 +0900

    feature: SSE 구독, 메시지 기능 구현

---
+++ src/main/java/com/supercoding/hanyipman/controller/SseTestController.java
@@ -0,0 +1,36 @@
+package com.supercoding.hanyipman.controller;
+
+import com.supercoding.hanyipman.dto.user.CustomUserDetail;
+import com.supercoding.hanyipman.dto.vo.SseTestRequest;
+import com.supercoding.hanyipman.dto.vo.SseTestResponse;
+import com.supercoding.hanyipman.dto.vo.Response;
+import com.supercoding.hanyipman.enums.EventName;
+import com.supercoding.hanyipman.service.SseService;
+import com.supercoding.hanyipman.utils.ApiUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/sse")
+public class SseTestController {
+    private final SseService sseService;
+
+    @GetMapping
+    public SseEmitter registerEmitter(@AuthenticationPrincipal CustomUserDetail auth)  {
+        return sseService.registerEmitter(auth.getUserId());
+    }
+
+
+    // TODO: sse 메시지 전송 예시 코드
+    @PostMapping
+    public Response<?> sendMessage(
+            @RequestBody SseTestRequest request,
+            @AuthenticationPrincipal CustomUserDetail auth) {
+        sseService.validSendMessage(auth.getUserId(), EventName.NOTICE_ORDER, new SseTestResponse(request.getTitle(), request.getContent()));
+        return ApiUtils.success(HttpStatus.OK, "메시지가 전송됐습니다.", null);
+    }
+}
---
+++ src/main/java/com/supercoding/hanyipman/dto/vo/SseTestResponse.java
@@ -0,0 +1,13 @@
+package com.supercoding.hanyipman.dto.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SseTestResponse {
+    private String title;
+    private String content;
+}
