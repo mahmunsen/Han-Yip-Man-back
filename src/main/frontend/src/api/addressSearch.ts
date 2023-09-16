b8ea7b90e32cfb1d2f50deebee6dc0b06cd233bf
no.oneho <oneho.dev@gmail.com>
2023-09-16 12:35:07 +0900

    Refactor: 리액트 프로젝트 통합 배포 프로젝트파일이 아닌 빌드파일만 보게 수정

--- src/main/frontend/src/api/addressSearch.ts
+++
@@ -1,7 +0,0 @@
-const ps = new window.kakao.maps.services.Places()
-
-const searchAddressByKeyword = (keyword: string, callback: any) => {
-  ps.keywordSearch(keyword, callback)
-}
-
-export default searchAddressByKeyword
--- src/main/frontend/src/components/RestaurantsManagement/ManagementContainer/ManagementContainer.style.ts
+++
@@ -1,6 +0,0 @@
-import styled from '@emotion/styled'
-
-export const Wrapper = styled.div`
-  flex: 1;
-  height: 100%;
-`
