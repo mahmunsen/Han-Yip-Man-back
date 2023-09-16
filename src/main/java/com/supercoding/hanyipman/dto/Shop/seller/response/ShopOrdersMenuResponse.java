66aa22557596759419476020254d64918c4f7e37
G1-H <hwangjiwon1992@gmail.com>
2023-09-16 14:14:23 +0900

    Refactor : 스웨거 코드 수정
    불필요 어노테이션 제거

--- src/main/java/com/supercoding/hanyipman/dto/Shop/seller/response/ShopOrdersMenuResponse.java
+++ src/main/java/com/supercoding/hanyipman/dto/Shop/seller/response/ShopOrdersMenuResponse.java
@@ -1,7 +1,6 @@
 package com.supercoding.hanyipman.dto.Shop.seller.response;
 
 import com.supercoding.hanyipman.entity.Cart;
-import io.swagger.annotations.ApiModelProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,9 +16,7 @@ import java.util.stream.Collectors;
 @Builder
 @Schema(description = "주문 메뉴 메뉴 응답 DTO")
 public class ShopOrdersMenuResponse {
-    @ApiModelProperty(value="주문 메뉴 이름 필드", dataType = "String")
     public String menuName;
-    @ApiModelProperty(value="주문 메뉴 옵션 리스트 필드", dataType = "List<String>")
     public List<String> optionNames;
 
     public static ShopOrdersMenuResponse from(Cart cart) {
