d323f302f56657d8c18fdbd8ba79b5ccde5232f4
김주환 <kjh71047@naver.com>
2023-09-20 01:16:39 +0900

    feat: yml파일 카카오 RedirectUri변경
    yml파일 카카오 RedirectUri변경

--- src/main/java/com/supercoding/hanyipman/service/KakaoOauthService.java
+++ src/main/java/com/supercoding/hanyipman/service/KakaoOauthService.java
@@ -28,9 +28,8 @@ public class KakaoOauthService {
     private String kakaoClientId;
     @Value("${kakao.kakaoSecretKey}")
     private String kakaoSecretKey;
-    //    @Value("${kakao.kakaoRedirectUri}")
-//    private String kakaoRedirectUri;
-    private String kakaoRedirectUri = "http://54.180.103.214:8080/auth/kakao";
+    @Value("${kakao.kakaoRedirectUri}")
+    private String kakaoRedirectUri;
 
     private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
