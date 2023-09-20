2dbb5f713e06258dc9d43e6b7f8383ef3326baf7
Woojin Lee <69797420+zmfpdl64@users.noreply.github.com>
2023-09-20 18:19:08 +0900

    refactor: CorsFilter 수정
    Sse 통신시 해당 설정에서 cors가 발생해서 allowOriginPattern으로 수정

--- src/main/java/com/supercoding/hanyipman/config/security/CorsFilterConfig.java
+++ src/main/java/com/supercoding/hanyipman/config/security/CorsFilterConfig.java
@@ -9,22 +9,15 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsFilterConfig{
+public class CorsFilterConfig implements WebMvcConfigurer{
 
-    @Bean
-    public CorsConfiguration corsConfiguration() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("*"); // 모든 오리진 허용
-        configuration.addAllowedHeader("*"); // 모든 헤더 허용
-        configuration.addExposedHeader("X-API-VERSION"); // 커스텀 헤더를 노출시킴
-        configuration.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
-        return configuration;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .exposedHeaders("X-API-VERSION")
+                .allowedOriginPatterns("*")
+                .allowedHeaders("*");
     }
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfiguration());
-        return new CorsFilter(source);
-    }
 }
