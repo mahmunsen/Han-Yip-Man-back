f4cddb8c6727cbfca2176237eaeb2a930000294e
Woojin Lee <69797420+zmfpdl64@users.noreply.github.com>
2023-09-21 02:51:24 +0900

    refactor: 주문 요청 반환값 변경

---
+++ src/main/java/com/supercoding/hanyipman/dto/order/response/OrderIdResponse.java
@@ -0,0 +1,10 @@
+package com.supercoding.hanyipman.dto.order.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class OrderIdResponse {
+    private Long orderId;
+}
