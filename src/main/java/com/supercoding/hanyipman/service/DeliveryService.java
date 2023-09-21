f74f0f15638bc6ab996b2ecd638a1a6b88717f6a
Woojin Lee <69797420+zmfpdl64@users.noreply.github.com>
2023-09-22 00:50:34 +0900

    refactor: 전체 장바구니 조회 커서조회 -> 전체조회
    cursor 기반으로 한다면 size가 10으로 설정 됐을 때 담은 장바구니가 20개 면 10개 먼저 가져오고 추후에 드래그 했을 때 10개를 더 가져오는 방식이라 소비자 입장에서는 당황스러울 수 있을것 같다고 생각했습니다.
    기존에 하신 로직이 문제가 없다면
    저는 지금 로직으로 해도 괜찮습니다

--- src/main/java/com/supercoding/hanyipman/service/DeliveryService.java
+++ src/main/java/com/supercoding/hanyipman/service/DeliveryService.java
@@ -18,11 +18,13 @@ import com.supercoding.hanyipman.repository.order.EmOrderRepository;
 import com.supercoding.hanyipman.repository.order.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.Optional;
+import java.util.concurrent.*;
 import java.util.stream.IntStream;
 
 import static com.supercoding.hanyipman.enums.EventName.NOTICE_DRON_LOCATION;
@@ -34,39 +36,42 @@ import static com.supercoding.hanyipman.enums.EventName.NOTICE_ORDER_BUYER;
 @Service
 public class DeliveryService {
     private final BuyerRepository buyerRepository;
-    private final OrderService orderService;
     private final EmOrderRepository orderRepository;
     private final SseMessageService sseMessageService;
-    private final AddressRepository addressRepository;
+    private final ExecutorService threadPools = new ThreadPoolExecutor(20, 50, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+
     private final Integer END_TIME = 60;
 
+    @Async
     @Transactional(readOnly = true)
     public void getDeliveryLocation(Long userId, Long orderId) {
-        Order order = findOrderByOrderId(orderId);
-        Address startAddress = order.getShop().getAddress();
-        Address endAddress = order.getAddress();
-        findBuyerByUserId(userId);
+        threadPools.submit(() -> {
+            Order order = findOrderByOrderId(orderId);
+            Address startAddress = order.getShop().getAddress();
+            Address endAddress = order.getAddress();
+            findBuyerByUserId(userId);
 
-        // 1차 구현 시작지점, 끝 지점 둘 다 +, + 라 가정하고 작성 추후 변경 예정
-        Double startLatitude = startAddress.getLatitude();
-        Double startLongitude = startAddress.getLongitude();
-        Double endLatitude = endAddress.getLatitude();
-        Double endLongitude = endAddress.getLongitude();
+            // 1차 구현 시작지점, 끝 지점 둘 다 +, + 라 가정하고 작성 추후 변경 예정
+            Double startLatitude = startAddress.getLatitude();
+            Double startLongitude = startAddress.getLongitude();
+            Double endLatitude = endAddress.getLatitude();
+            Double endLongitude = endAddress.getLongitude();
 
-        Double moveLatitude = Math.abs(startLatitude - endLatitude);;
-        Double moveLongitude = Math.abs(startLongitude - endLongitude);
-        if(startLatitude > endLatitude) moveLatitude = -moveLatitude;
-        if(startLongitude > endLongitude) moveLongitude = -moveLongitude;
-
-        for(int i = 0; i < END_TIME; i++) {
-            DeliveryLocation deliveryLocation = new DeliveryLocation(startLatitude + moveLatitude / END_TIME * i, startLongitude + moveLongitude / END_TIME * i);
-            sseMessageService.sendSse(SendSseResponse.of(userId, deliveryLocation, NOTICE_DRON_LOCATION.getEventName()));
-            try{
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                log.info("드론 배달 위치 전송 중 에러가 발생했습니다.");
+            Double moveLatitude = Math.abs(startLatitude - endLatitude);;
+            Double moveLongitude = Math.abs(startLongitude - endLongitude);
+            if(startLatitude > endLatitude) moveLatitude = -moveLatitude;
+            if(startLongitude > endLongitude) moveLongitude = -moveLongitude;
+            for(int i = 0; i < END_TIME; i++) {
+                DeliveryLocation deliveryLocation = new DeliveryLocation(startLatitude + moveLatitude / END_TIME * i, startLongitude + moveLongitude / END_TIME * i);
+                sseMessageService.sendSse(SendSseResponse.of(userId, deliveryLocation, NOTICE_DRON_LOCATION.getEventName()));
+                try{
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    log.info("드론 배달 위치 전송 중 에러가 발생했습니다.");
+                }
             }
-        }
+        });
+
         //TODO: 배달이 끝나고 배달 완료 메시지 반환
     }
 
